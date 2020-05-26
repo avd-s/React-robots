d250e30a779983dbb48bb368b274fbb4588f7fdd
Aravind <53109884+avd-s@users.noreply.github.com>
2020-05-27 01:15:54 +0530

    update fix

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));      // renders App.js File on screen using virtual React DOM
 registerServiceWorker();
