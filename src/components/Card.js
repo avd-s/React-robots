7895bae7a099ad010c00227deaec21af299ff04f
Aravind <53109884+avd-s@users.noreply.github.com>
2020-05-27 01:09:37 +0530

    update fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
-  return (
+const Card = ({ name, email, id }) => {                   // React props
+  return (                                                                         // Explicit API call to get robot's images according to name
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />    
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
