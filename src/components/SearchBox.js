cb77bdf941854b8b4916891f6259f17546299aaf
Aravind <53109884+avd-s@users.noreply.github.com>
2020-05-27 01:11:35 +0530

    update fix

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange }) => {
+const SearchBox = ({ searchfield, searchChange }) => {      //using React props
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba b--green bg-lightest-blue'          // Tachyons styling
         type='search'
         placeholder='search robots'
-        onChange={searchChange}
+        onChange={searchChange}                               // invoke fn in parent component
       />
     </div>
   );
